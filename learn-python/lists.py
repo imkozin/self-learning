c00a4dc9cb8b3fa2a6737814d11ebece7f90eeb0
imkozin <ivan.kozin@hotmail.com>
2024-01-22 23:27:24 +0300

    solved tasks

--- learn-python/lists.py
+++ learn-python/lists.py
@@ -71,13 +71,59 @@ def is_lucky(num):
     else:
         print('Билет несчастливый')
 
-is_lucky(123006)
+# is_lucky(123006)
 
 # I)
 # Напишите программу, которая из введенной пользователем строки создаст список. Удалить из этого списка все буквы ‘a’, ‘e’, ‘u’. Строка вводится на английском и символы для удаления так же на английском языке.
+def check_str():
+    user_input = input('Enter a string: ')
+    if not user_input.isascii():
+        print('User input contains wrong characters')
+        return
 
+    res = []
 
+    for char in user_input:
+        if char not in ['a', 'e', 'u']:
+            res.append(char) 
+
+    print(res)
+
+# check_str()
 # II)
 # Напишите программу, которая создаст список на 10 элементов, заполненный целыми числами, введенными с клавиатуры. Удалите все элементы, которые меньше 5 и найдите среднее арифметическое всех оставшихся элементов.
+
+def check_num():
+    user_input = input('Enter at least 10 numbers separated by spaces: ')
+    nums = [int(num) for num in user_input]
+    print(nums)
+
+    if len(nums) < 10:
+        print('List is too short')
+        return
+    
+    filtered_nums = [num for num in nums if num >= 5]
+
+    if not filtered_nums:
+        print('There are no numbers greater than or equal to 5.')
+        return
+
+    average = sum(filtered_nums) / len(filtered_nums)
+
+    print(f'The average of numbers greater than or equal to 5 is: {average}')
+
+# check_num()
 # III)
 # Напишите программу, которая создаст список на 10 элементов, заполненный целыми числами (положительными и отрицательными), введенными с клавиатуры. Отсортировать список в порядке убывания модулей чисел и вывести его на экран. Также найти произведение элементов кратных 3, имеющих четный индекс.
+    
+def check_nums():
+    user_input = input('Enter 10 numbers: ')
+    nums = [int(num) for num in user_input.split()]
+
+    nums.sort(key=abs)
+
+    res = [num for i, num in enumerate(nums) if i % 2 != 0 and num % 3 == 0]
+    print(res)
+
+check_nums()
+
