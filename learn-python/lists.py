806c804ee8785459fb79b4daad834b861a8607c3
imkozin <ivan.kozin@hotmail.com>
2024-02-05 19:28:44 +0300

    learned ajax

--- learn-python/lists.py
+++ learn-python/lists.py
@@ -125,5 +125,100 @@ def check_nums():
     res = [num for i, num in enumerate(nums) if i % 2 != 0 and num % 3 == 0]
     print(res)
 
-check_nums()
+# check_nums()
+
+# a = [1, 2, 3, 4, 5]
+# for x in range(5):
+#     a[x] = a[x] * a[x]
+#     print(a[x])
+
+# A = [0] * 1000
+# top = 0
+# x = int(input())
+# while x != 0:
+#     A[top] = x
+#     top += 1
+#     x = int(input())
+# for k in range(4,-1, -1):
+#     print(A[k])
+
+# N = int(input())
+# A = [0] * N
+# B = [0] * N
+# for k in range(N):
+#     A[k] = int(input())
+# for k in range(N):
+#     B[k] = A[k]
+# C = A
+
+# def array_search(A:list, N:int, x:int):
+#     '''Осуществляет поиск числа х в массиве А от 0-ого до N-1 индекса включительно. 
+#     Возвращает индекс элемента х в массиве А. Или -1 если такого нет.
+#     Если в массиве несколько одинаковых элементов х, то вернуть первый'''
+#     for k in range(N):
+#          if A[k] == x:
+#              return k
+#     return -1
+
+# def test_array_search():
+#     A1 = [1,2,3,4,5]
+#     m = array_search(A1, 5, 8)
+#     if m == -1:
+#         print('Test 1 passed')
+#     else:
+#         print('Test 1 failed')
+
+#     A2 = [-1,-2,-3,-4,-5]
+#     m = array_search(A2, 5, -3)
+#     if m == 2:
+#         print('Test 2 passed')
+#     else:
+#         print('Test 2 failed')
+
+#     A3 = [10,20,30,10,10]
+#     m = array_search(A3, 5, 10)
+#     if m == 0:
+#         print('Test 3 passed')
+#     else:
+#         print('Test 3 failed')
+
+# test_array_search()
+    
+def invert_array(A:list, N:int):
+    for k in range(N//2):
+        A[k], A[N - 1 - k] = A[N - 1 - k], A[k]
+
+
+def test_invert_array():
+    A1 = [1,2,3,4,5]
+    print(A1)
+    invert_array(A1, 5)
+    print(A1)
+    if A1 == [5,4,3,2,1]:
+        print('Test 1 passed')
+    else:
+        print('Test 1 failed')
+
+    A2 = [0, 0, 0, 0, 0, 0, 0, 10]
+    print(A2)
+    invert_array(A2, 8)
+    print(A2)
+    if A2 == [10, 0, 0, 0, 0, 0, 0, 0]:
+        print('Test 2 passed')
+    else:
+        print('Test 2 failed')
+
+# test_invert_array()
+N = int(input())        
+A = [True] * N
+
+A[0] = A[1] = False
+for k in range(2, N):
+    if A[k]:
+        for m in range(2*k, N, k):
+            A[m] = False
+
+print(A)
+for k in range(N):
+    print(k, "-простое" if A[k] else "-составное")
 
